a3bf0253e5b07e6aca7eceaa86e698af61a94e4d
Joshua <joshdavenport416@hotmail.com>
2019-10-30 16:44:55 -0700

    Starts and adds an image to the article

--- index.php
+++ index.php
@@ -5,6 +5,17 @@
 <!-- end banner div -->
 
 <main>
+<h1>Database Administrator Job Outlook in Seattle</h1>
+<p>According to indeed.com the average salary for a Database 
+Administrator is $56.37 per hour in Seattle, WA, which is 
+10% above the national average. Salary estimates are based 
+on 14 salaries submitted anonymously to Indeed by Database 
+Administrator employees, users, and collected from past and 
+present job advertisements on Indeed in the past 36 months. 
+The typical tenure for a Database Administrator is 1-3 years.</p>
+<img class="article" src="images/salary.jpg" alt="salary">
+
+
 </main>
 
 <aside>
