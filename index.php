559fca055fdcf23909976a5135880357d1308744
Joshua <joshdavenport416@hotmail.com>
2019-10-30 17:22:23 -0700

    Adds more to article

--- index.php
+++ index.php
@@ -6,15 +6,32 @@
 
 <main>
 <h1>Database Administrator Job Outlook in Seattle</h1>
-<p>According to indeed.com the average salary for a Database 
-Administrator is $56.37 per hour in Seattle, WA, which is 
-10% above the national average. Salary estimates are based 
-on 14 salaries submitted anonymously to Indeed by Database 
-Administrator employees, users, and collected from past and 
-present job advertisements on Indeed in the past 36 months. 
-The typical tenure for a Database Administrator is 1-3 years.</p>
+<P class="contributers">Contributers: George Allison, Crystal Brodrick, Joshua Davenport, & Samuel Tegegn </p>
+<h2>Projected Growth</h2>
+<p>According to <a href="https://www.indeed.com/salaries/database-administrator-Salaries,-Seattle-WA">indeed.com</a> 
+the average salary for a Database Administrator is $56.37 per hour in Seattle, WA, which is 10% above the national average. 
+Salary estimates are based on 14 salaries submitted anonymously to Indeed by Database Administrator employees, users, and 
+collected from past and present job advertisements on Indeed in the past 36 months. The typical tenure for a Database 
+Administrator is 1-3 years.</p>
 <img class="article" src="images/salary.jpg" alt="salary">
-
+<p>Employment of database administrators is projected to grow 9 percent from 2018 to 2028, faster than the average for 
+all occupations, According the the <a href="https://www.bls.gov/ooh/computer-and-information-technology/database-administrators.htm">
+Bureau of Labor Statistics.</a> Growth in this occupation will be driven by the increased data needs of companies across the economy.</p>
+<h2>Job Description</h2>
+<p><a href="https://www.zippia.com/database-administrator-jobs/">Database administrators</a> use specialized software to store and 
+organize data, such as financial information and customer shipping records. They make sure that data are available to users and 
+are secure from unauthorized access.</p>
+<h2>Education</h2>
+<p><a href="https://www.zippia.com/database-administrator-jobs/">Database administrators</a> &#40;DBAs&#41; usually have a bachelor’s degree 
+in an information- or computer-related subject such as computer science. Before becoming an administrator, these workers typically get 
+work experience in a related field.</P>
+<h2>Career Path</h2>
+<p>Most do not begin their careers as database administrators. Many first work as database developers or data analysts. 
+A database developer is a type of software developer who specializes in creating databases. The job of a data analyst is 
+to interpret the information stored in a database in a way the firm can use. Depending on their specialty, data analysts 
+can have different job titles, including financial analyst, market research analyst, and operations research analyst. After 
+mastering one of these fields, they may become a database administrator. For more information, see the profiles on software 
+developers, financial analysts, market research analysts, and operations research analysts.</P>
 
 </main>
 
