1c848aaef666a227a762d2b2486a201d37faba81
George Allison <49288165+joji-harada@users.noreply.github.com>
2019-11-05 15:29:03 -0800

    added config.php-changed styles-added history.php

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,23 @@
+<?php include('includes/config.php') ?>
 <!doctype html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<title>Seattle 2020 Database Careers</title>
-<!-- link styles below -->
-<link href="css/styles.css" type="text/css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <title>Seattle 2020 Database Careers</title>
+    <!-- link styles below -->
+    <link href="css/styles.css" type="text/css" rel="stylesheet">
 
 </head>
-<body>
+
+<body class="<?php echo $body ?>">
+    <div class="header">
+        <h1>Database Administrator Job Outlook in Seattle</h1>
+        <p class="contributers mobile-vanish">Contributers: George Allison, Crystal Brodrick, Joshua Davenport, and Samuel Tegegn</p>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="index.php">MAIN</a></li>
+            <li><a href="history.php">HISTORY</a></li>
+        </ul>
+    </nav>
